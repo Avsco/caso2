981d6a035de0c5d762b2fea178443d53e2cae67a
Alan Benavides <bruno@coastt.org>
2023-12-11 23:19:44 -0400

    change: conclusion message

--- src/screens/history.py
+++ src/screens/history.py
@@ -48,8 +48,18 @@ def item_history(item):
                 f"Cojinete varianza: {item['variance_f']}",
             ),
             html.div(
-                {'style':{"color": "#122A4C"}},
-                f"La simulación tiene una probabilidad mayor a 85% de interferencias, se recomienda reducir la varianza y/o medida la flecha o incrementar la varianza y/o medida del cojinete" if (item['total_interferences'] > 0 and item['total_simulations'] / item['total_interferences'] > 0.85) else f"La simulación tiene una probabilidad mayor a 40% y menor al 85% de interferencias, se recomienda reducir la varianza y/o medida la flecha o incrementar la varianza y/o medida del cojinete" if (item['total_interferences'] > 0 and item['total_simulations']/ item['total_interferences'] > 0.20) else f"La simulación tiene una probabilidad menor al 20%, se tiene una buena media y varianza entre la flecha y la media"
+                {"style": {"color": "#122A4C"}},
+                f"La simulación tiene una probabilidad mayor a 85% de interferencias, se recomienda reducir la varianza y/o medida de la flecha o incrementar la varianza y/o medida del cojinete"
+                if (
+                    item["total_interferences"] > 0
+                    and item["total_simulations"] / item["total_interferences"] > 0.85
+                )
+                else f"La simulación tiene una probabilidad mayor a 40% y menor al 85% de interferencias, se recomienda reducir la varianza y/o medida de la flecha o incrementar la varianza y/o medida del cojinete"
+                if (
+                    item["total_interferences"] > 0
+                    and item["total_simulations"] / item["total_interferences"] > 0.20
+                )
+                else f"La simulación tiene una probabilidad menor a 20% de interferencias, se tiene una buena media y varianza entre la flecha y el cojinete",
             ),
         ),
     )
